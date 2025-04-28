ed12d26dba8f6029b20b6b8515248a606f310960
Meghna Chandra <meghnachandra@Meghnas-MacBook-Air-2.local>
2025-04-28 17:39:04 +0530

    Initial setup, added all dependencies

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Simple route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// MongoDB Connection
+const connectDB = require('./config/db');
+connectDB();
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
