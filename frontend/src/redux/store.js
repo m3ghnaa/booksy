fbc96f00df779eb072203d7696fd01d3cb1e10cc
Meghna Chandra <meghnachandra@Meghnas-MacBook-Air-2.local>
2025-05-14 23:37:45 +0530

    fix redux thunk error

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 
 import authReducer from './authSlice';
 import userReducer from './userSlice';
